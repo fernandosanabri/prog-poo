bf45bc7056aebcf18ee8313df6153d1894de8392
fernandosanabri <50969960+fernandosanabri@users.noreply.github.com>
2019-06-12 21:08:29 +0200

    clase4

--- main.java
+++ main.java
@@ -1,3 +1,5 @@
+import clase_04.clase4;
+
 public class main {
     public static void main(String[] args) {
         programa1 temp = new programa1();
@@ -6,13 +8,18 @@ public class main {
         programa1 nm = new programa1();
         programa1 com = new programa1();
         programa1 tiem = new programa1();
-
+              clase4 prueba = new clase4();
+              clase4 prueba2 = new clase4();
+              clase4 men = new clase4();
         nm.nombre();
         temp.mostrar();
         ed.edad_mos();
         n2.entrada();
         com.comercio();
         tiem.tiempos();
+             prueba.calcular_edad();
+             prueba2.clarification_edad();
+             men.menu();
 
     }
 }
