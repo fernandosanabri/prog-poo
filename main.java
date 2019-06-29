abfd1fc5e5ea83b8dde0686c7a9976740f6313e3
fernandosanabri <50969960+fernandosanabri@users.noreply.github.com>
2019-06-29 07:29:33 +0200

    parcial
    parcial

--- main.java
+++ main.java
@@ -1,4 +1,10 @@
-import taller2.casos2;
+//import clase5.monedero;
+//import taller2.casos2;
+//import taller2.casos2;
+import Parcial1_problema1.Problema1;
+import Parcial1_problema1.Problema2;
+import Parcial1_problema1.Problema3;
+import Parcial1_problema1.Problema4;
 
 public class main {
     public static void main(String[] args) {
@@ -21,19 +27,34 @@ public class main {
              prueba.calcular_edad();
              prueba2.clarification_edad();
              */
-             casos2 prob2 = new casos2();
-        casos2 vcl = new casos2();
-        casos2 for1 = new casos2();
-        casos2 cas2 = new casos2();
-        casos2 tmp = new casos2();
-        casos2 mn =new casos2();
-        prob2.pais();
-       cas2.banco();
-       vcl.vocal();
-        tmp.desarr();
-        mn.menu2();
+            // casos2 prob2 = new casos2();
+       // casos2 vcl = new casos2();
+       // casos2 for1 = new casos2();
+      //  casos2 cas2 = new casos2();
+      //  casos2 tmp = new casos2();
+      //  casos2 mn =new casos2();
+     //   prob2.pais();
+       // cas2.banco();
+      // vcl.vocal();
+     //   tmp.desarr();
+     //   mn.menu2();
+        // men.menu();
+       // String emp1 = "empresa pepito";
+       // monedero cliente1 = new monedero;
 
-            // men.menu();
+        Problema1 correrprograma = new Problema1();
+        correrprograma.ejecutar();
+
+        Problema2 correrprograma2 = new Problema2();
+        correrprograma2.parcial();
+
+        Problema3 correrprograma3 = new Problema3();
+        correrprograma3.ejecutar();
+
+        Problema4 correrprograma4 = new Problema4();
+        correrprograma4.ejecutar();
+
+
 
     }
 }
